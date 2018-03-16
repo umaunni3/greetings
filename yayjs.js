5d10748a5229c96c598f44281eaf892c744440be
Uma Unni <umaunni@berkeley.edu>
2018-03-16 02:19:09 -0700

    added looping functionality to button dialogue

--- yayjs.js
+++ yayjs.js
@@ -12,14 +12,23 @@ function clickCounter() {
 var memer = clickCounter();
 
 function meme() {
+    if (document.getElementById('hai').innerHTML === "i'm so r ry") {
+        /* ok u r forgiven; reset counter */
+        memer = clickCounter();
+        document.getElementById('ooh').innerHTML = "ok i forgive u";
+        document.getElementById('hai').innerHTML = 'click me u wont';
+    }
     var x = memer();
+
     if (x === 1) {
         document.getElementById('ooh').style.display = "block";
         document.getElementById('hai').innerHTML = "oh shit u did";
     }
     if (x > 12) {
         document.getElementById('ooh').innerHTML = "too much click !!";
+        document.getElementById('hai').innerHTML = "i'm so r ry";
     } else {
-    document.getElementById('ooh').innerHTML = x;
+        // reset = false;
+        document.getElementById('ooh').innerHTML = x;
     }
 }
